771e321c0dc00114d720ba3a4991195e7c3e834d
aryayanroy <aryayanroy0@gmail.com>
2023-07-23 22:36:09 +0530

    initial commit

---
+++ admin/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel | Our Hearts Don't Beat. They Rev...</title>
+    <link rel="shortcut icon" href="../assets/public/branding/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <style>
+        :root{
+            font-size: 14px;
+        }
+    </style>
+</head>
+<body>
+    <header class="sticky-top">
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a href="#" class="navbar-brand"><img src="../assets/public/branding/srijan-logo.png" alt="Team Srijan" height=16></a>
+                <div class="collapse navbar-collapse flex-grow-0">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-wrench me-2"></i><span>Admin</span></button>
+                            <ul class="dropdown-menu">
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-warehouse fa-sm me-2"></i><span>Garage</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-people-group fa-sm me-2"></i><span>Crews</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-hand-holding-dollar fa-sm me-2"></i><span>Crowdfunding</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-gear me-2"></i><span>Settings</span></a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-solid fa-pen me-2"></i><span>Member</span></button>
+                            <ul class="dropdown-menu">
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-house fa-sm me-2"></i><span>Home</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-regular fa-newspaper fa-sm me-2"></i><span>Updates</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-indian-rupee-sign fa-sm me-2"></i><span>Sponsors</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-trophy fa-sm me-2"></i><span>Competitions</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-flag-checkered me-2"></i><span>Milestones</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-list-ul me-2"></i><span>Overview</span></a></li>
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-flag-checkered me-2"></i><span>Milestones</span></a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item"><a href="history" class="btn btn-dark"><i class="fa-solid fa-clock-rotate-left me-2"></i><span>Activity</span></a></li>
+                        <li class="nav-item dropdown">
+                            <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown"><i class="fa-regular fa-circle-user me-2"></i><span>Hailee Steinfeld</span></button>
+                            <ul class="dropdown-menu">
+                                <li><a href="profile" class="dropdown-item"><i class="fa-solid fa-user fa-sm me-2"></i><span>Profile</span></a></li>
+                                <li><a href="settings" class="dropdown-item"><i class="fa-solid fa-gear me-2"></i><span>Settings</span></a></li>
+                                <li><a href="logout" class="dropdown-item"><i class="fa-solid fa-arrow-right-from-bracket me-2"></i><span>Logout</span></a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="container-xxl">
+    </main>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</html>
