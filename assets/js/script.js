b032278a8350de9d7f59d484eef00b1b8a5ca2ab
aryayanroy <aryayanroy0@gmail.com>
2024-07-06 14:51:11 +0530

    init

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,9 +24,9 @@ $(document).ready(function(){
     $(window).scroll(function(){
         let scroll = $(this).scrollTop();
         if(scroll == 0){
-            header.css("box-shadow","none");
+            header.removeClass("shadow");
         }else{
-            header.css("box-shadow","0 6px 8px rgba(0, 0, 0, 0.4)");
+            header.addClass("shadow");
         }
     })
 
