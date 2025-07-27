956f07dd522fdd9e0a467fc276c4972d2a603f5b
aryayanroy <aryayanroy0@gmail.com>
2025-07-27 20:53:19 +0530

    init

--- assets/js/crews.js
+++ assets/js/crews.js
@@ -1,28 +1,81 @@
-$(document).ready(function(){
-    $(".crews-carousel").owlCarousel({
-        margin:10,
-        responsive:{
-            0:{
-                items:1,
-                stagePadding: 25,
-                loop:true,
-                autoplay:true,
-                autoplayTimeout: 3000,
-                autoplayHoverPause:true
-            },
-            425:{
-                items:2
-            },
-            576:{
-                items:3,
-                stagePadding: 25
-            },
-            768:{
-                items:4
-            },
-            992:{
-                items:5
-            }
+$(document).ready(function () {
+        const filename = "/assets/json/members.json";
+        const $batches = $("#batches");
+        const $teams = $("#teams");
+        const $members = $("#members");
+
+        function error_print(msg){
+            $members.html(`<div class="col-12"><p class="text-center">${msg}</p></div>`);
         }
-    })
-})
+
+        $.get(filename, function (data) {
+            
+            function load_options($element, key) {
+                const options = data[key];
+                if (!options) return;
+                for (let i in options) {
+                    const option = new Option(options[i],i);
+                    $element.append(option);
+                }
+            }
+
+            load_options($batches, "batches");
+            load_options($teams, "teams");
+
+            function card(member){
+
+                const image = `/assets/images/members/${member.image}`;
+
+                let teams = `<p class="card-text d-flex flex-wrap">`;
+                for(const key of member.team){
+                    const team = data.teams[key];
+                    teams += `<small class="badge rounded-pill text-bg-danger m-1">${team}</small>`;
+                }
+                teams += `</p>`;
+
+                let contacts = "";
+                for (key in member.contacts){
+                    const contact = data.contact_methods[key];
+                    contacts += `<a href="${contact.url+member.contacts[key]}" class="nav-item" target="_blank"><i class="${contact.icon} fa-xl"></i></a>`;
+                }
+
+                return `<div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="card h-100">
+                        <div class="ratio ratio-1x1"><img src="${image}" alt="${member.name}" class="card-img-top"></div>
+                        <div class="card-body d-flex flex-column justify-content-between">
+                            <h4 class="card-title">${member.name}</h4>
+                            ${teams}
+                            <nav class="nav nav-justified">${contacts}</nav>
+                        </div>
+                    </div>
+                </div>`
+            }
+
+            function load_members() {
+                const batch = $batches.val();
+                const team = $teams.val();
+
+                $members.empty();
+                let member_found = false;
+                for (const member of data.members) {
+                    if (member.batch == batch && member.team.includes(team)) {
+                        $members.append(card(member))
+                        member_found = true;
+                    }
+                }
+                if (!member_found) {
+                    error_print("No members found.");
+                }
+            }
+
+            load_members();
+
+            $batches.add($teams).on("change", function(){
+                load_members();
+            });
+
+        }).fail(function (jqxhr, textStatus, error) {
+            console.error("Failed to load", filename, "=>", textStatus, error);
+            error_print("Failed to load members.");
+        });
+    });
