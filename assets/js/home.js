417ea38c0a1624f544f15f8a072d9a0498f71b9c
aryayanroy <aryayanroy0@gmail.com>
2025-08-20 18:26:12 +0530

    init

--- assets/js/home.js
+++ assets/js/home.js
@@ -23,17 +23,4 @@ $(document).ready(function(){
             }
         }
     })
-
-    var final = new Date("1 22, 2025 00:00:00").getTime();
-    convert = 1000*60*60;
-    setInterval(function(){
-        var now = new Date().getTime();
-        var distance = final - now;
-        if(distance > 0){
-            $("#days").text(Math.floor(distance / (convert * 24)));
-            $("#hrs").text(Math.floor((distance % (convert * 24)) / (convert)));
-            $("#mins").text(Math.floor((distance % (convert)) / (convert/60)));
-            $("#secs").text(Math.floor((distance % (convert/60)) / 1000));
-        }
-    }, 1000)
 })
